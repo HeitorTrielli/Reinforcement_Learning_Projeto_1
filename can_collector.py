43682c95824724c7d2bd9c793d692f165f36c66d
= <=>
2025-08-31 14:11:18 -0300

    algum  progresso no agente robo

--- can_collector.py
+++ can_collector.py
@@ -103,30 +103,11 @@ class RobotState:
 
 class RobotAgent:
     def __init__(self, step_size=0.1, epsilon=0.1):
-        self.state = RobotState()
-
-    def search(self):
-        pass
-
-    def wait(self):
-        pass
-
-    def recharge(self):
-        pass
-
-    def act(self):
-        pass
-
-
-# AI player
-class Player:
-    def __init__(self, step_size=0.1, epsilon=0.1):
         self.estimations = dict()
         self.step_size = step_size
         self.epsilon = epsilon
         self.states = []
         self.greedy = []
-        self.symbol = 0
 
     def reset(self):
         self.states = []
@@ -136,22 +117,6 @@ class Player:
         self.states.append(state)
         self.greedy.append(True)
 
-    def set_symbol(self, symbol):
-        self.symbol = symbol
-        for hash_val in all_states:
-            state, is_end = all_states[hash_val]
-            if is_end:
-                if state.winner == self.symbol:
-                    self.estimations[hash_val] = 1.0
-                elif state.winner == 0:
-                    # we need to distinguish between a tie and a lose
-                    self.estimations[hash_val] = 0.5
-                else:
-                    self.estimations[hash_val] = 0
-            else:
-                self.estimations[hash_val] = 0.5
-
-    # update value estimation
     def backup(self):
         """
         Update the value estimation of the states in reverse order. Using the temporal difference method,
@@ -167,27 +132,17 @@ class Player:
             )
             self.estimations[state] += self.step_size * td_error
 
-    # choose an action based on the state
     def act(self):
         state = self.states[-1]
         next_states = []
         next_positions = []
-        for i in range(BOARD_ROWS):
-            for j in range(BOARD_COLS):
-                if state.data[i, j] == 0:
-                    next_positions.append([i, j])
-                    next_states.append(state.next_state(i, j, self.symbol).hash())
 
-        if np.random.rand() < self.epsilon:
-            action = next_positions[np.random.randint(len(next_positions))]
-            action.append(self.symbol)
-            self.greedy[-1] = False
-            return action
+        valid_actions = state.get_valid_actions()
 
         values = []
         for hash_val, pos in zip(next_states, next_positions):
             values.append((self.estimations[hash_val], pos))
-        # to select one of the actions of equal value at random due to Python's sort is stable
+
         np.random.shuffle(values)
         values.sort(key=lambda x: x[0], reverse=True)
         action = values[0][1]
@@ -205,35 +160,6 @@ class Player:
             "policy_%s.bin" % ("first" if self.symbol == 1 else "second"), "rb"
         ) as f:
             self.estimations = pickle.load(f)
-
-
-# human interface
-# input a number to put a chessman
-# | q | w | e |
-# | a | s | d |
-# | z | x | c |
-class HumanPlayer:
-    def __init__(self, **kwargs):
-        self.symbol = None
-        self.keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
-        self.state = None
-
-    def reset(self):
-        pass
-
-    def set_state(self, state):
-        self.state = state
-
-    def set_symbol(self, symbol):
-        self.symbol = symbol
-
-    def act(self):
-        self.state.print_state()
-        key = input("Input your position:")
-        data = self.keys.index(key)
-        i = data // BOARD_COLS
-        j = data % BOARD_COLS
-        return i, j, self.symbol
 
 
 def train(epochs, print_every_n=500):
@@ -262,20 +188,6 @@ def train(epochs, print_every_n=500):
 
 # The game is a zero sum game. If both players are playing with an optimal strategy, every game will end in a tie.
 # So we test whether the AI can guarantee at least a tie if it goes second.
-def play():
-    while True:
-        player1 = HumanPlayer()
-        player2 = Player(epsilon=0)
-        judger = Judger(player1, player2)
-        player2.load_policy()
-        winner = judger.play()
-        if winner == player2.symbol:
-            print("You lose!")
-        elif winner == player1.symbol:
-            print("You win!")
-        else:
-            print("It is a tie!")
-
 
 if __name__ == "__main__":
     train(int(1e5))
